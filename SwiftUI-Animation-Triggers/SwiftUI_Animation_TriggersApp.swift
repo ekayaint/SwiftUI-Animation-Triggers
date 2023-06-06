abb6740d0f666762633f16323fd92d7677cb57df
ekayaint <ekayaint@gmail.com>
2023-06-06 19:41:43 +0300

    Initial Commit

---
+++ SwiftUI-Animation-Triggers/SwiftUI_Animation_TriggersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Animation_TriggersApp.swift
+//  SwiftUI-Animation-Triggers
+//
+//  Created by ekayaint on 6.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Animation_TriggersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
